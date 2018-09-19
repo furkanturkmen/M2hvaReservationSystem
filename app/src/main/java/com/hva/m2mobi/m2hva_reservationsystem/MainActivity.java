d4a74db797ca25379d111d0fb052f19a6d434efa
Furkan Türkmen <furkan.turkmen@hva.nl>
2018-09-19 15:20:40 +0200

    Initial commit

---
+++ app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hva.m2mobi.m2hva_reservationsystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
