c4b2597de50602d5b533efc02bb5f706ccb71e0b
Kyle Watson <kylewatson98@gmail.com>
2018-09-26 11:42:39 +0200

    Deleted intent
    tab activity wont come up so we can change that to come up when login button is pressed

--- app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/MainActivity.java
+++ app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, TabActivity.class);
-        startActivity(intent);
     }
 }
