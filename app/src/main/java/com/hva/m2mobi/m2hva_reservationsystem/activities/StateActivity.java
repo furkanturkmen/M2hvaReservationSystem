4709dc9faf59addc867b79c4cab86569ddaf0972
Baran <baran.altin@hva.nl>
2018-10-16 16:44:53 +0200

    Button + strings added and state activity

---
+++ app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/activities/StateActivity.java
@@ -0,0 +1,16 @@
+package com.hva.m2mobi.m2hva_reservationsystem.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hva.m2mobi.m2hva_reservationsystem.R;
+
+public class StateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.no_bookings);
+
+    }
+}
