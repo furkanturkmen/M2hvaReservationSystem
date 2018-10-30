aef790dbd82fad57d37ceec9c847c85ddc12fcd5
Kim van Dueren den Hollander <Kim.van.Dueren.den.Hollander@hva.nl>
2018-10-30 16:40:49 +0100

    added details to reservation object

--- app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/adapters/Reservation.java
+++ app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/adapters/Reservation.java
@@ -1,19 +1,54 @@
 package com.hva.m2mobi.m2hva_reservationsystem.adapters;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class Reservation {
-    private String description;
+    private String title;
+    private ArrayList<String> attendees = new ArrayList<String>();
 
+    //formatters for date and time
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
+    private DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_LOCAL_TIME;
+    private String formattedDate = dateFormatter.format(LocalDate.now());
+    private String formattedTime = timeFormatter.format(LocalDate.now());
 
-    public Reservation(String description) {
-        this.description = description;
+    private String reservationDate = formattedDate;
+    private String startTime = formattedTime;
+    private String endTime = formattedTime;
 
+    //reservation object
+    public Reservation(String title, ArrayList attendees, String reservationDate, String startTime, String endTime) {
+        this.title = title;
+        this.attendees = attendees;
+        this.reservationDate = reservationDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
-    public String getDescription() {
-        return description;
+    //getters for all variables
+    public String getTitle() {
+        return title;
+    }
+    public String getReservationDate(){
+        return reservationDate;
+    }
+    public String getStartTime(){
+        return startTime;
+    }
+    public String getEndTime(){
+        return endTime;
+    }
+    public ArrayList getAttendees(){
+        return attendees;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
