2f3295f2c882090afb4a63872be42e085dae78d5
Kim van Dueren den Hollander <Kim.van.Dueren.den.Hollander@hva.nl>
2018-12-11 16:49:07 +0100

    start time can't be after the end time

--- app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/utils/CustomTimePickerDialog.java
+++ app/src/main/java/com/hva/m2mobi/m2hva_reservationsystem/utils/CustomTimePickerDialog.java
@@ -11,12 +11,16 @@ import java.lang.reflect.Field;
 public class CustomTimePickerDialog extends TimePickerDialog {
     private int minMinute;
     private int minHour;
+    private int maxMinute;
+    private int maxHour;
     private NumberPicker minuteSpinner;
 
-    public CustomTimePickerDialog(Context context, OnTimeSetListener listener, int hourOfDay, int minute) {
+    public CustomTimePickerDialog(Context context, OnTimeSetListener listener, int hourOfDay, int minute, int maxHour, int maxMinute) {
         super(context, android.R.style.Theme_Holo_Light_Dialog_NoActionBar, listener, hourOfDay, minute, true);
         this.minMinute = minute;
         this.minHour = hourOfDay;
+        this.maxMinute = maxMinute;
+        this.maxHour = maxHour;
     }
 
     @Override
@@ -31,11 +35,15 @@ public class CustomTimePickerDialog extends TimePickerDialog {
             minuteSpinner = timePicker
                     .findViewById(field.getInt(null));
             minuteSpinner.setMinValue(minMinute);
+            if (minHour == maxHour){
+                minuteSpinner.setMaxValue(maxMinute);
+            }
 
             Field hourField = classForid.getField("hour");
 
             NumberPicker hourSpinner = timePicker.findViewById(hourField.getInt(null));
             hourSpinner.setMinValue(minHour);
+            hourSpinner.setMaxValue(maxHour);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -50,6 +58,11 @@ public class CustomTimePickerDialog extends TimePickerDialog {
             minuteSpinner.setMinValue(0);
         } else {
             minuteSpinner.setMinValue(minMinute);
+        }
+        if (hourOfDay < maxHour){
+            minuteSpinner.setMaxValue(59);
+        } else {
+            minuteSpinner.setMaxValue(maxMinute);
         }
     }
 }
